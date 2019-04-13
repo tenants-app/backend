b9de00f601cf1943c59e756437c7df2db22c5261
Filip Fonal <filipfonal@gmail.com>
2019-04-13 19:33:55 +0200

    Changes in routing

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import AuthController from '../controllers/auth';
+import Validator from '../validators/requests';
 const router = express.Router();
 
 
-router.post('/login', AuthController.login);
-router.post('/register', AuthController.register);
+router.post('/login', Validator.loginRequest, AuthController.login);
+router.post('/register', Validator.registerRequest, AuthController.register);
 
 
 export default router;
