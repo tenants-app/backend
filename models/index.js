369dcbacb4f5d652c14b15defcb09f1b9366dfbb
Filip Fonal <filipfonal@gmail.com>
2019-04-13 19:28:52 +0200

    Added exporting models index file

---
+++ models/index.js
@@ -0,0 +1,9 @@
+import User from './User';
+import Group from './Group';
+import GroupActivationLink from './GroupActivationLink';
+
+export default {
+    User, 
+    Group, 
+    GroupActivationLink
+}
